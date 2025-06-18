00476c74877b8802596176dc866522663e025e97
Firman1803 <firmanhidayahtullah@students.unnes.ac.id>
2025-06-18 09:00:11 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Prediksi Pilihan Kampus", layout="wide")
+st.title("🎓 Aplikasi Prediksi Pemilihan Kampus oleh Mahasiswa")
+st.markdown("Gunakan sidebar untuk navigasi halaman.")
